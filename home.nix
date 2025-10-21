db74632b59d9cdac98c77682c2416f6017434e65
atorizva <atoriz@users.noreply.github.com>
2025-10-20 21:28:45 -0600

    task: zsh + starship prompt

--- home.nix
+++ home.nix
@@ -33,6 +33,10 @@
     uv
     coursier
 
+    # Shell
+    zsh
+    starship # prompt config
+
     # Utils
     direnv
     eza
@@ -52,17 +56,11 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    # ZSH + Starship
+    ".zshrc".source = dotfiles/zsh/.zshrc;
+    ".config/starship.toml".source = dotfiles/starship/starship.toml;
+    # Zed
     ".config/zed/settings.json".source = dotfiles/zed/settings.json;
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # Home Manager can also manage your environment variables through
